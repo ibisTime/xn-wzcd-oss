e1a6e0d95e62aeb7a8752c99b41b92f64778481e
U55W8NHWI71RNAP\Administrator <544741767@qq.com>
2018-07-02 18:43:32 +0800

    update

--- src/container/history/historyBusiness/historyBusiness.js
+++ src/container/history/historyBusiness/historyBusiness.js
@@ -103,7 +103,7 @@ class HistoryBusiness extends React.Component {
             fields,
             pageCode: 630520,
             searchParams: {
-                curNodeCodeList: ['021_18', '021_19', '005_02', '005_04']
+                curNodeCodeList: ['021_20', '021_19', '005_02', '005_04']
             }
         });
     }
