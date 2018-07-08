caf5e369637c9d5ae070331d339ce9635513c899
hss1994 <1070798770@qq.com>
2018-07-08 15:43:34 +0800

    登录页，首页

---
+++ src/api/home.js
@@ -0,0 +1,12 @@
+import fetch from 'common/js/fetch';
+import { getUserId, getRoleCode, getTeamCode } from 'common/js/util';
+
+// 分页查询我的公告
+export function getPageMyNotice() {
+  return fetch(632728, { userId: getUserId(), start: 1, limit: 5 });
+}
+
+// 分页查询我的公司制度
+export function getPageMyCompanysystem() {
+    return fetch(632738, { userId: getUserId(), start: 1, limit: 5 });
+}
--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,61 +1,75 @@
 import React from 'react';
-import { Form, Icon, Input, Button } from 'antd';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { login } from '@redux/user';
+import {Form, Icon, Input, Button} from 'antd';
+import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {login} from '@redux/user';
 import './login.css';
+// import logoWhite from '../../images/logo-white.png';
 
 const FormItem = Form.Item;
 
 @connect(
-  state => state.user,
-  { login }
+    state => state.user,
+    {login}
 )
 class Login extends React.Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        // this.setState({ loading: true })
-        this.props.login(values);
-      }
-    });
-  }
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div>
-        {this.props.redirectTo ? <Redirect to={this.props.redirectTo}/> : null}
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <FormItem>
-            {getFieldDecorator('loginName', {
-              rules: [{
-                required: true,
-                message: '请输入用户名!'
-              }]
-            })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
-            )}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('loginPwd', {
-              rules: [{
-                required: true,
-                message: '请输入密码!'
-              }]
-            })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
-            )}
-          </FormItem>
-          <FormItem>
-            <Button type="primary" htmlType="submit" loading={this.props.fetching} className="login-form-button">
-              登录
-            </Button>
-          </FormItem>
-        </Form>
-      </div>
-    );
-  }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.props.login(values);
+            }
+        });
+    }
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <div className="login-body">
+                <div className='logo-wrap'>
+                    {/* <img src={logoWhite}/> */}
+                    <p>后台管理系统</p>
+                </div>
+                <div className="login-wrap">
+                    <div className="title">用户登录</div>
+                    {this.props.redirectTo ? <Redirect to={this.props.redirectTo}/> : null}
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <FormItem className="form-item">
+                            <div className="icon icon-user"></div>
+                            {getFieldDecorator('loginName', {
+                                rules: [{
+                                    required: true,
+                                    message: '请输入用户名!'
+                                }]
+                            })(
+                                <Input placeholder="用户名"/>
+                            )}
+                        </FormItem>
+                        <FormItem className="form-item">
+                            <div className="icon icon-pwd"></div>
+                            {getFieldDecorator('loginPwd', {
+                                rules: [{
+                                    required: true,
+                                    message: '请输入密码!'
+                                }]
+                            })(
+                                <Input type="password"
+                                       placeholder="密码"/>
+                            )}
+                        </FormItem>
+                        <div className="remember-wrap">
+                        </div>
+                        <FormItem className="button-wrap">
+                            <Button type="primary" htmlType="submit" loading={this.props.fetching}
+                                    className="login-form-button">
+                                立即登录
+                            </Button>
+                        </FormItem>
+                    </Form>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Form.create()(Login);
