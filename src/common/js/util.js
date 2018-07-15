889c29c648b33da151ca22a38ea3bff78af94ec4
U55W8NHWI71RNAP\Administrator <544741767@qq.com>
2018-07-15 20:14:10 +0800

    update

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -304,25 +304,25 @@ export function convertCurrency(currencyDigits) {
   currencyDigits = moneyReplaceComma(currencyDigits);
   var MAXIMUM_NUMBER = 99999999999.99;
   // Predefine the radix characters and currency symbols for output:
-  var CN_ZERO = "零";
-  var CN_ONE = "壹";
-  var CN_TWO = "贰";
-  var CN_THREE = "叁";
-  var CN_FOUR = "肆";
-  var CN_FIVE = "伍";
-  var CN_SIX = "陆";
-  var CN_SEVEN = "柒";
-  var CN_EIGHT = "捌";
-  var CN_NINE = "玖";
-  var CN_TEN = "拾";
-  var CN_HUNDRED = "佰";
-  var CN_THOUSAND = "仟";
-  var CN_TEN_THOUSAND = "万";
-  var CN_HUNDRED_MILLION = "亿";
-  var CN_DOLLAR = "元";
-  var CN_TEN_CENT = "角";
-  var CN_CENT = "分";
-  var CN_INTEGER = "整";
+  var CN_ZERO = '零';
+  var CN_ONE = '壹';
+  var CN_TWO = '贰';
+  var CN_THREE = '叁';
+  var CN_FOUR = '肆';
+  var CN_FIVE = '伍';
+  var CN_SIX = '陆';
+  var CN_SEVEN = '柒';
+  var CN_EIGHT = '捌';
+  var CN_NINE = '玖';
+  var CN_TEN = '拾';
+  var CN_HUNDRED = '佰';
+  var CN_THOUSAND = '仟';
+  var CN_TEN_THOUSAND = '万';
+  var CN_HUNDRED_MILLION = '亿';
+  var CN_DOLLAR = '元';
+  var CN_TEN_CENT = '角';
+  var CN_CENT = '分';
+  var CN_INTEGER = '整';
   var integral; // Represent integral part of digit number.
   var decimal; // Represent decimal part of digit number.
   var outputCharacters; // The output result.
@@ -338,29 +338,29 @@ export function convertCurrency(currencyDigits) {
   var quotient,
     modulus;
   currencyDigits = currencyDigits.toString();
-  if (currencyDigits == "") {
-    alert("请输入小写金额！");
-    return "";
+  if (currencyDigits === '') {
+    alert('请输入小写金额！');
+    return '';
   }
-  if (currencyDigits.match(/[^,.\d]/) != null) {
-    alert("小写金额含有无效字符！");
-    return "";
+  if (currencyDigits.match(/[^,.\d]/) !== null) {
+    alert('');
+    return '';
   }
   if ((currencyDigits).match(/^((\d{1,3}(,\d{3})*(.((\d{3},)*\d{1,3}))?)|(\d+(.\d+)?))$/) == null) {
-    alert("小写金额的格式不正确！");
-    return "";
+    alert('小写金额的格式不正确！');
+    return '';
   }
-  currencyDigits = currencyDigits.replace(/,/g, ""); // Remove comma delimiters.
-  currencyDigits = currencyDigits.replace(/^0+/, ""); // Trim zeros at the beginning.
+  currencyDigits = currencyDigits.replace(/,/g, ''); // Remove comma delimiters.
+  currencyDigits = currencyDigits.replace(/^0+/, ''); // Trim zeros at the beginning.
   // Assert the number is not greater than the maximum number.
   if (Number(currencyDigits) > MAXIMUM_NUMBER) {
-    alert("金额过大，应小于1000亿元！");
-    return "";
+    alert('金额过大，应小于1000亿元！');
+    return '';
   }
 
   // Process the coversion from currency digits to characters:
   // Separate integral and decimal parts before processing coversion:
-  parts = currencyDigits.split(".");
+  parts = currencyDigits.split('.');
   if (parts.length > 1) {
     integral = parts[0];
     decimal = parts[1];
@@ -368,15 +368,15 @@ export function convertCurrency(currencyDigits) {
     decimal = decimal.substr(0, 2);
   } else {
     integral = parts[0];
-    decimal = "";
+    decimal = '';
   }
   // Prepare the characters corresponding to the digits:
-  digits = new Array(CN_ZERO, CN_ONE, CN_TWO, CN_THREE, CN_FOUR, CN_FIVE, CN_SIX, CN_SEVEN, CN_EIGHT, CN_NINE);
-  radices = new Array("", CN_TEN, CN_HUNDRED, CN_THOUSAND);
-  bigRadices = new Array("", CN_TEN_THOUSAND, CN_HUNDRED_MILLION);
-  decimals = new Array(CN_TEN_CENT, CN_CENT);
+  digits = [CN_ZERO, CN_ONE, CN_TWO, CN_THREE, CN_FOUR, CN_FIVE, CN_SIX, CN_SEVEN, CN_EIGHT, CN_NINE];
+  radices = ['', CN_TEN, CN_HUNDRED, CN_THOUSAND];
+  bigRadices = ['', CN_TEN_THOUSAND, CN_HUNDRED_MILLION];
+  decimals = [CN_TEN_CENT, CN_CENT];
   // Start processing:
-  outputCharacters = "";
+  outputCharacters = '';
   // Process integral part if it is larger than 0:
   if (Number(integral) > 0) {
     zeroCount = 0;
@@ -385,7 +385,7 @@ export function convertCurrency(currencyDigits) {
       d = integral.substr(i, 1);
       quotient = p / 4;
       modulus = p % 4;
-      if (d == "0") {
+      if (d === '0') {
         zeroCount++;
       } else {
         if (zeroCount > 0) {
@@ -394,7 +394,7 @@ export function convertCurrency(currencyDigits) {
         zeroCount = 0;
         outputCharacters += digits[Number(d)] + radices[modulus];
       }
-      if (modulus == 0 && zeroCount < 4) {
+      if (modulus === 0 && zeroCount < 4) {
         outputCharacters += bigRadices[quotient];
         zeroCount = 0;
       }
@@ -402,22 +402,21 @@ export function convertCurrency(currencyDigits) {
     outputCharacters += CN_DOLLAR;
   }
   // Process decimal part if there is:
-  if (decimal != "") {
+  if (decimal !== '') {
     for (i = 0; i < decimal.length; i++) {
       d = decimal.substr(i, 1);
-      if (d != "0") {
+      if (d !== '0') {
         outputCharacters += digits[Number(d)] + decimals[i];
       }
     }
   }
   // Confirm and return the final output string:
-  if (outputCharacters == "") {
+  if (outputCharacters === '') {
     outputCharacters = CN_ZERO + CN_DOLLAR;
   }
-  if (decimal == "") {
+  if (decimal === '') {
     outputCharacters += CN_INTEGER;
   }
-  outputCharacters = outputCharacters;
   return outputCharacters;
 }
 /**
